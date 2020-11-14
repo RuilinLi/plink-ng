fdc02d0765ec1cfe116ce17bf59f75c32cc15d65
RuilinLi <ruilinli@stanford.edu>
2020-11-14 22:12:12 +0000

    plinkmatrix from pgen

--- 2.0/pgenlibr/R/allclasses.R
+++ 2.0/pgenlibr/R/allclasses.R
@@ -1,11 +1,11 @@
 setClassUnion("matrixOrNULL", c("matrix", "NULL"))
-
+setClassUnion("charOrNULL", c("character", "NULL"))
 
 setClass("PlinkMatrix", representation(ptr = "externalptr", samples = "integer", 
-    variants = "integer", fname = "character", xim = "numeric", Dim = "integer", ncov = "integer", covs="matrixOrNULL"))
+    variants = "integer", fname = "character", xim = "numeric", Dim = "integer", ncov = "integer", covs="matrixOrNULL", colname="charOrNULL"))
 
 #' @export
-PlinkMatrix <- function(fname, samples, variants, covs=NULL) {
+PlinkMatrixbase <- function(fname, samples, variants, covs=NULL) {
     if (is.unsorted(samples, strictly = TRUE)) {
         stop("The subset indices must be sorted in strictly increasing order.")
     }
@@ -28,7 +28,7 @@ PlinkMatrix <- function(fname, samples, variants, covs=NULL) {
 
     
     new("PlinkMatrix", ptr = new("externalptr"), samples = samples, variants = variants, fname = fname, 
-        Dim = dimensions, xim = 0, ncov = ncov, covs = covs
+        Dim = dimensions, xim = 0, ncov = ncov, covs = covs, colname=NULL
       )
 }
 setGeneric("actualize", function(x) standardGeneric("actualize"))
@@ -51,15 +51,15 @@ setMethod(
 )
 
 #' @export
-PlinkMatrixFromPgen <- function(pgen, variants, covs=NULL){
+PlinkMatrixFromPgen <- function(pgen, variants, covs=NULL, variant_names=NULL){
     if (length(variants) != length(unique(variants))) {
         stop("Must not have duplicated variant index.")
     }
-    sample_size = 0L
+    sample_size = integer(1)
     xim = double(length(variants))
     xptr = getcompactptrfromPgen(pgen, variants, xim, sample_size)
     dimensions <- c(sample_size, length(variants))
-    ncov = 0L
+    ncov <- integer(1)
     if(!is.null(covs)) {
         if(nrow(covs) != dimensions[1]) {
             stop("Covariates must have same number of rows as SNP Matrix")
@@ -67,8 +67,17 @@ PlinkMatrixFromPgen <- function(pgen, variants, covs=NULL){
         ncov = ncol(covs)
         dimensions[2] = dimensions[2] + ncov
     }
-    new("PlinkMatrix", ptr = xptr, samples = 0L, variants = variants, fname = "fromPgen", 
-        Dim = dimensions, xim = xim, ncov = ncov, covs = covs
+    colname = NULL
+    if(!is.null(variant_names)){
+        cov_names = colnames(covs)
+        if((!is.null(covs)) && is.null(cov_names)){
+            cov_names = paste0("V", 1:ncol(covs))
+        }
+        colname = c(cov_names, variant_names)
+
+    }
+    new("PlinkMatrix", ptr = xptr, samples = integer(1), variants = variants, fname = "fromPgen", 
+        Dim = dimensions, xim = xim, ncov = ncov, covs = covs, colname=colname
       )
 }
 
@@ -86,6 +95,11 @@ setMethod(
         x@covs = newcov
         x@Dim[2] = x@Dim[2] + ncol(newcov)
         x@ncov = ncol(newcov)
+        cov_names = colnames(newcov)
+        if(is.null(cov_names)){
+            cov_names = paste0("V", 1:ncol(newcov))
+        }
+        x@colname = c(cov_names, x@colname)
         return(x)
     }
 )
